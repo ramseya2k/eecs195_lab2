0cf72604eb4fbb217d7720e851e27cde83a7870e
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2023-05-13 14:51:55 -0700

    Update redux.py

--- src/TurtleBot/src/redux.py
+++ src/TurtleBot/src/redux.py
@@ -66,58 +66,58 @@ class PID:
         self.goal_theta = data.data[2]
         self.mode = data.data[3]
 
-    def euclidean_distance(self, goal_pose):
+    def euclidean_distance(self):
         """Euclidean distance between current pose and the goal."""
-        return sqrt(pow((goal_pose[0] - self.pose_x), 2) + pow((goal_pose[1] - self.pose_y), 2))
+        return sqrt(pow((self.goal_x - self.pose_x), 2) + pow((self.goal_y - self.pose_y), 2))
 
-    def steering_angle(self, goal_pose):
-        return atan2(goal_pose[1] - self.pose_y, goal_pose[0] - self.pose_x)
+    def steering_angle(self):
+        return atan2(self.goal_y - self.pose_y, self.goal_x - self.pose_x)
 
-    def proportional_linear_vel(self, goal_pose):
-        return min(self.Kp_gain_linear * self.euclidean_distance(goal_pose), 0.1)
+    def proportional_linear_vel(self):
+        return min(self.Kp_gain_linear * self.euclidean_distance(), 0.1)
 
-    def derivative_linear_vel(self, goal_pose):
-        derivate_linear_vel = (self.euclidean_distance(goal_pose) - self.error_prior_linear) / self.time_discr
+    def derivative_linear_vel(self):
+        derivate_linear_vel = (self.euclidean_distance() - self.error_prior_linear) / self.time_discr
         ud_linear = self.Kd_gain_linear * derivate_linear_vel
-        self.error_prior_linear = self.euclidean_distance(goal_pose)  # update the error prior 
+        self.error_prior_linear = self.euclidean_distance()  # update the error prior 
         return ud_linear
 
-    def integral_linear_vel(self, goal_pose):
-        integral_linear_vel = self.integral_prior_linear + self.euclidean_distance(goal_pose) * self.time_discr
+    def integral_linear_vel(self):
+        integral_linear_vel = self.integral_prior_linear + self.euclidean_distance() * self.time_discr
         # update the integral prior 
         ui_linear = self.Ki_gain_linear * integral_linear_vel
 	self.integral_prior_linear = integral_linear_vel
         return ui_linear
 
-    def proportional_angular_vel(self, goal_pose): 
-        error_angle = self.steering_angle(goal_pose) - self.pose_theta
+    def proportional_angular_vel(self): 
+        error_angle = self.steering_angle() - self.pose_theta
         return min(self.Kp_gain_angular * error_angle, 0.1)
 
-    def derivative_angular_vel(self, goal_pose):
-        error_angle = self.steering_angle(goal_pose) - self.pose_theta
+    def derivative_angular_vel(self):
+        error_angle = self.steering_angle() - self.pose_theta
         derivative_angular_vel = (error_angle - self.error_prior_angular) / self.time_discr
         ud_angular = self.Kd_gain_angular * derivative_angular_vel
         # update the error prior
 	self.error_prior_angular = error_angle 
         return ud_angular
 
-    def integral_angular_vel(self, goal_pose):
-        error_angle = self.steering_angle(goal_pose) - self.pose_theta
+    def integral_angular_vel(self):
+        error_angle = self.steering_angle() - self.pose_theta
         integral_angular_vel = self.integral_prior_angular +error_angle * self.time_discr
         ui_angular = self.Ki_gain_angular * integral_angular_vel
         # update the integral prior 
         self.integral_prior_angular = integral_angular_vel
         return ui_angular
 
-    def PID_controller_linear(self, goal_pose):
-        pid_control_linear = self.proportional_linear_vel(goal_pose) + self.derivative_linear_vel(goal_pose) + self.integral_linear_vel(goal_pose)
+    def PID_controller_linear(self):
+        pid_control_linear = self.proportional_linear_vel() + self.derivative_linear_vel() + self.integral_linear_vel()
         # set error priors for linear to 0 
         self.error_prior_linear = 0
         self.integral_prior_linear = 0
         return pid_control_linear
 
-    def PID_controller_angular(self, goal_pose):
-        pid_control_angular = self.proportional_angular_vel(goal_pose) + self.derivative_angular_vel(goal_pose) + self.integral_angular_vel(goal_pose)
+    def PID_controller_angular(self):
+        pid_control_angular = self.proportional_angular_vel() + self.derivative_angular_vel() + self.integral_angular_vel()
         # update error priors for angular to 0
         self.error_prior_angular = 0
         self.integral_prior_angular = 0
@@ -127,9 +127,9 @@ class PID:
         while not rospy.is_shutdown():
                 vel_msg = Twist()
                 if self.mode == 1:
-                        while self.euclidean_distance(goal_pose) >= .05:
-                                vel_msg.linear.x = self.PID_controller_linear(goal_pose)
-                	        vel_msg.angular.z = self.PID_controller_angular(goal_pose)
+                        while self.euclidean_distance() >= .05:
+                                vel_msg.linear.x = self.PID_controller_linear()
+                	        vel_msg.angular.z = self.PID_controller_angular()
                 	        self.velocity_publisher.publish(vel_msg)
                 	        self.rate.sleep()
 			vel_msg.linear.x = 0
