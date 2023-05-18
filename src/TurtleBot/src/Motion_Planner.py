56dc137849ee358e4d55e1360fa0a6fe2e9cd034
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2023-05-17 22:31:48 -0700

    Update Motion_Planner.py

--- src/TurtleBot/src/Motion_Planner.py
+++ src/TurtleBot/src/Motion_Planner.py
@@ -100,10 +100,10 @@ def main():
 	rospy.init_node('Motion_Planner', anonymous=False)
 	pub = rospy.Publisher('/start_goal', Float64MultiArray, queue_size=10)
 	while not rospy.is_shutdown():
+		rospy.Subscriber('/trajectory', Float64MultiArray, trajectory_callback)
 		pub.publish(send_info()) # sends the start and goal coordinates to /start_goal
 			#rompt_flag = False
 		rate = rospy.Rate(10)
-		rospy.Subscriber('/trajectory', Float64MultiArray, trajectory_callback)
 		rate.sleep()
 
 if __name__ == '__main__':
