772fa7bed545990b051e9331069fc00a63225bd8
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2023-05-20 12:58:50 -0700

    Update Motion_Planner.py

--- src/TurtleBot/src/Motion_Planner.py
+++ src/TurtleBot/src/Motion_Planner.py
@@ -8,7 +8,6 @@ from geometry_msgs.msg import PoseStamped
 
 goal_position = None # bound to be a global variable
 trajectory = [] # initialized to be empty, global
-trajectory_index = 0
 current_position_x = 0.0
 current_position_y = 0.0
 
@@ -28,32 +27,25 @@ def current_position(msg): # from /gazebo/model_states
 	global current_position_x, current_position_y
 	current_position_x = msg.pose[1].position.x
 	current_position_y = msg.pose[1].position.y
-	rospy.loginfo("Current position: x={}, y={}".format(current_position_x, current_position_y))
+	#rospy.loginfo("Current position: x={}, y={}".format(current_position_x, current_position_y))
 		
 def trajectory_callback(msg): # send the first point in the trajectory to the PID and publish to reference_pose topic
 	# monitor the pose of the robot using using the /Gazebo/model_states topic 
 	# once it gets very close to that position, the node should send the second position in the trajectory and wait until the robot is very close until it visits all the points
-	global trajectory, trajectory_index
+	global trajectory
 	trajectory = []
-	trajectory_index = 0
 	for i in range(0, len(msg.data), 2):
 		x = msg.data[i]
 		y = msg.data[i+1]
 		trajectory.append([x, y])
-	rospy.loginfo(trajectory) 
-	if len(trajectory) > 0:
-		traj_temp = trajectory[0] # trajectory contains 2 elements in 1 element, separate it
-		x = traj_temp[0]
-		y = traj_temp[1]
-		reference_pose_pub.publish(Float64MultiArray(data=[x, y, 0, 1])) # x, y, theta, mode
+	#rospy.loginfo(trajectory) 
 
 def monitor_robot_pose():
 	
 	global trajectory, trajectory_index, current_position_x, current_position_y
 	while not trajectory: # waits until there is a trajectory 
-		rospy.sleep(0.1) 
-	print(trajectory)
-	
+		rospy.sleep(0.1)
+	rospy.loginfo(trajectory) 
 	for i in range(len(trajectory)):
 		traj_temp = trajectory[i]
 		x = traj_temp[0]
@@ -61,31 +53,23 @@ def monitor_robot_pose():
 		reference_pose_pub.publish(Float64MultiArray(data=[x, y, 0, 1])) #x, y, theta, mode
 		rospy.loginfo("Moving to point ({}, {})".format(x, y))
 	
-		while distance() >= 0.05:
+		while sqrt((trajectory[i][0] - current_position_x)**2 + (trajectory[i][1] - current_position_y)**2) >= 0.05:
 			rospy.sleep(0.1)
-			print("Distance: ", distance())
-		trajectory_index = i
-		print("Trajectory index: ", trajectory_index)
+			print("Distance: ", sqrt((trajectory[i][0] - current_position_x)**2 + (trajectory[i][1] - current_position_y)**2))
+		print("Trajectory index: ", i)
 	rospy.loginfo("Reached the goal!\n") 
-
-
-def distance():
-	global current_position_x, current_position_y, trajectory, trajectory_index
-	trajectory_point = trajectory[trajectory_index]
-	return sqrt((trajectory_point[0] - current_position_x)**2 + (trajectory_point[1] - current_position_y)**2)
 
 if __name__ == '__main__':
 	rospy.init_node('Motion_Planner', anonymous=False)
-	print("Ready!\n")
+	rospy.loginfo("Ready!\n")
 	start_goal_pub = rospy.Publisher('/start_goal', Float64MultiArray, queue_size=10)
 	reference_pose_pub = rospy.Publisher('/reference_pose', Float64MultiArray, queue_size=10)
 	rospy.Subscriber('/gazebo/model_states', ModelStates, current_position) # obtain position of robot
 	rospy.Subscriber('/trajectory', Float64MultiArray, trajectory_callback)
 	
-	rospy.wait_for_message('/target_pose', PoseStamped) # waits for the first goal
+	#rospy.wait_for_message('/target_pose', PoseStamped) # waits for the first goal
 	rospy.Subscriber('/target_pose', PoseStamped, goal_position_callback) # goal position
-	while not rospy.is_shutdown():
-		monitor_robot_pose()
+	monitor_robot_pose()
 
 '''
 # THIS IS FOR PART 2
