4a70b1da64952eedb3b1de951358000769dca20f
ramseya2k <49705855+ramseya2k@users.noreply.github.com>
2023-05-20 11:44:38 -0700

    Update Motion_Planner.py

--- src/TurtleBot/src/Motion_Planner.py
+++ src/TurtleBot/src/Motion_Planner.py
@@ -59,7 +59,7 @@ def monitor_robot_pose():
 		rospy.loginfo("Moving to point ({}, {})".format(x, y))
 		while distance() >= 0.05:
 			rospy.sleep(0.1)
-			print("Distance: ", distance(trajectory[trajectory_index]))
+			print("Distance: ", distance())
 			current_position_x = rospy.wait_for_message('/gazebo/model_states', ModelStates).pose[1].position.x
 			current_position_y = rospy.wait_for_message('/gazebo/model_states', ModelStates).pose[1].position.y
 		trajectory_index +=1
